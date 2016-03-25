944547e0307322aca319851308d14389c712e787
Oleksandr Chekmez <chekmez@nikama.com>
2016-03-25 08:11:06 +0200

    add fontend source

---
+++ ArcelorMittal/obj/Release/Package/PackageTmp/Scripts/Shared/common.js
@@ -0,0 +1,137 @@
+﻿var serviceUrl = '../../ODataWebApp/api/Static/';
+var interval = 5000; //grid auto refresh interval (30 sec)
+var _intervalID; //initiate interval ID
+
+$(function () {
+
+    // if there is table name in hash
+    if (location.hash) {
+
+        // get item name from hash value
+        var item = location.hash.replace('#', '');
+
+        // load item by hash
+        vmLoadItem(item); 
+    };
+
+    //build grid from tables list    
+    $("ul#item_list").on("click", "a", function (e) {
+
+        //cancelling standard behavior of web link element
+        e.preventDefault();
+
+        // get item name
+        var item = $(this).attr('href');
+
+        // set hash value as current selected item
+        location.hash = item;
+
+        vmLoadItem(item);
+
+        if (_intervalID)
+            clearInterval(_intervalID);
+    });
+
+    $('div#grid').on("click", "*", function (e) {
+
+        if (_intervalID)
+            clearInterval(_intervalID);
+    });
+
+
+    $('div#grid').on('grid_is_loaded', function (e, table) {
+
+        autoRefreshGrid(table, interval);
+    });
+
+});
+
+function vmGetMetadata() {
+
+    // get metadata xml by service url
+    // service requires user credentials
+    return $.ajax({
+                url: serviceUrl + '$metadata',
+                xhrFields: {
+                    withCredentials: true
+                }
+            })            
+            // show alert message in case of error
+            .error(function () {
+                alert('failed to read metadata');
+            });
+};
+
+function vmGetActions(metadata) {
+
+    // find actions (action information)
+    // in metadata xml file
+    return $(metadata).find('Action')
+                .map(function (ind, entity) {
+
+                    var fields = $(entity).find('Parameter')
+                                        .map(function (idx, param) {
+
+                                            return {
+                                                name: $(param).attr('Name'),
+                                                type: $(param).attr('Type'),
+                                                mandatory: $(param).attr('Nullable')
+                                            };
+                                        });
+
+                    return {
+                        name: $(entity).attr('Name'),
+                        fields: fields
+                    };
+                });
+};
+
+function vmGetTables(metadata) {
+
+    // find entities (table information)
+    // in metadata xml file
+    return $(metadata).find('EntityType')
+                .map(function (ind, entity) {
+
+                    var fields = $(entity).find('Property')
+                                    .map(function (idx, param) {
+
+                                        return {
+                                            name: $(param).attr('Name'),
+                                            type: $(param).attr('Type'),
+                                            mandatory: $(param).attr('Nullable')
+                                        };
+                                    });
+
+                    var key = $(entity).find('Key')
+                                    .children('PropertyRef')
+                                    .attr('Name');
+
+                    return {
+                        name: $(entity).attr('Name'),
+                        key: key,
+                        fields: fields
+                    };
+                });
+};
+
+function vmPopulateList(items) {
+
+    var $ul = $('ul#item_list').empty();
+
+    items.each(function (ind, item) {
+
+        var $li = $('<li />').appendTo($ul);
+
+        $('<a />').attr('href', item.name)
+            .text(item.name)
+            .appendTo($li);
+    });
+};
+
+function vmLoadItem(name) {
+
+    // empty function
+    // should be overriden
+    // with specific load case
+};
---
+++ ArcelorMittal/obj/Release/Package/PackageTmp/Scripts/Table/index.js
@@ -0,0 +1,200 @@
+﻿$(function () {
+
+    vmGetMetadata()
+        .done(function (metadata) {
+
+            // read tables from metadata
+            var tables = vmGetTables(metadata);
+
+            // populate item list
+            vmPopulateList(tables);
+        });
+});
+
+function vmLoadItem(name) {
+
+    console.log('> Loading table: ' + name);
+
+    vmGetMetadata()
+        .done(function (metadata) {
+
+            // find action by name
+            var table = vmGetTables(metadata).filter(function (ind, table) {
+
+                return table.name == name;
+            })
+            .get(0);
+
+            vmBuildGrid(table);
+
+            $("div#grid").trigger('grid_is_loaded', table);
+        });
+};
+
+function vmBuildGrid(table) {
+
+    // clear grid
+    var $grid = $('div#grid').empty();
+
+    _$grid = $grid;
+
+    var fields = table.fields
+                    .map(function (ind, item) {
+
+                        var field = {
+                            name: item.name,
+                            width: vmGetWidth(item.name)
+                        };
+
+                        switch (item.type) {
+
+                            case 'Edm.Int32':
+                                field.type = 'number';
+                                break;
+                            case 'Edm.String':
+                                field.type = 'text';
+                                break;
+                        };
+
+                        return field;
+                    });
+
+    fields.push({ type: "control" });
+
+    $grid.jsGrid({
+        height: "500px",
+        width: "1000px",
+
+        sorting: true,
+        paging: true,
+        editing: true,
+        filtering: true,
+        autoload: true,
+        pageLoading: true,
+        inserting: true,
+        pageIndex: 1,
+        pageSize: 5,
+
+        controller: {
+            loadData: loadData,
+            insertItem: insertItem,
+            updateItem: updateItem,
+            deleteItem: deleteItem
+        },
+
+        fields: fields
+    });   
+
+    function loadData(filter) {
+
+        var data = {
+            $filter: vmGetFilter(filter, fields),
+            $count: true,
+            $top: filter.pageSize,
+            $skip: (filter.pageIndex - 1) * filter.pageSize
+        };
+       
+        return $.ajax({
+            url: serviceUrl + table.name,
+            dataType: "json",
+            data: data,
+        }).then(function (response) {
+
+            return {
+                itemsCount: response['@odata.count'],
+                data: response.value
+            };
+        });
+    };
+
+    function vmGetFilter(conditions, fields) {
+
+        var filter = [];
+       
+        for (field in conditions)
+            if (conditions[field]) {
+
+                var field = fields.toArray().find(function (x) {
+                    return x.name == field
+                });
+
+                if (field && field.type) {
+                    if (field.type == 'text')
+                        filter.push("contains({0},'{1}')".format(field.name, conditions[field.name]));
+                    else if (field.type == 'number')
+                        filter.push("{0} eq {1}".format(field.name, conditions[field.name]));
+                    else
+                        alert('Filtering by {0} not supported'.format(field.type));
+                }               
+            };
+
+        return filter.length > 1 ? filter.join(' and ') : filter[0];
+    };
+
+    function insertItem(item) {
+
+        return $.ajax({
+            url: serviceUrl + table.name,
+            type: "POST",
+            data: JSON.stringify(item),
+            contentType: "application/json;odata=verbose"
+        })
+        .fail(function () {
+            alert('Insert failed');
+        });
+    };
+
+    function updateItem(item) {
+
+        var id = item[table.key];
+
+        return $.ajax({
+            url: serviceUrl + table.name + '(' + id + ')',
+            type: "PUT",
+            data: JSON.stringify(item),
+            contentType: "application/json;odata=verbose"
+        })
+        .fail(function () {
+            alert('Update failed');
+        });
+    };
+
+    function deleteItem(item) {
+
+        var id = item[table.key];
+
+        return $.ajax({
+            type: "DELETE",
+            url: serviceUrl + table.name + '(' + id + ')'
+        })
+        .fail(function () {
+            alert('Delete failed');
+        });
+    };    
+};
+
+function vmGetWidth(name) {
+
+    if (name.length <= 15)
+        return "150px"
+    else if (name.length > 15 && name.length <= 25)
+        return "220px"
+    else
+        return "250px"
+}
+
+function autoRefreshGrid(table, interval) {
+
+    //set interval and every interval time
+    //read current situation for table data
+    _intervalID = setInterval(function () {
+
+        vmBuildGrid(table);
+        console.log('>refresh');
+    }, interval);
+}
+
+
+
+
+
