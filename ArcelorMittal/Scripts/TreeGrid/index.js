8e6f5f4c9c89be6730c9c4060623421a2f651825
Oleksandr Chekmez <chekmez@nikama.com>
2016-04-22 08:03:22 +0300

    #102 completed work on TreeGrid

--- ArcelorMittal/Scripts/TreeGrid/index.js
+++ ArcelorMittal/Scripts/TreeGrid/index.js
@@ -2,155 +2,43 @@
 
     $treeContainer = $('#jstree');
 
-    //params of create Tree method:
-    //1. container - DOM element used as container for tree
-    //2. table name - name of table whose data will be used to build tree
-    //3. list of tree params
-    // it's always array of 3 params - id, parentId and node name
-    //and each item of array is name of table field
-    //corresponding to these tree parameters
-    createTree($treeContainer, 'EquipmentClass', ['ID', 'ParentID', 'Description']);
-
-    function createTree(container, table, fields) {
-
-        $('.createNodeControls').hide();
-
-        $.get(serviceUrl + table).then(function (data) {
-
-            var treeData = data.value.map(function (item, index, arr) {
-
-                return {
-
-                    'id': item[fields[0]],
-                    'parent': item[fields[1]] == null ? '#' : item[fields[1]],
-                    'text': item[fields[2]]
-                };
-
-            });
-
-            container.jstree({
-                'core': {
-
-                    'data': treeData,
-                    'check_callback': true
-                },
-                check_callback: true
-            }).on('changed.jstree', function (e, data) {
-
-                container.trigger('tree-item-selected', { id: data.node.id });
-            });
-
-        });
-    };
-
+    $treeContainer.odataTree({
+        serviceUrl: '../../ODataRestierDynamic/api/Dynamic/',
+        table: 'EquipmentClass',
+        keys: {
+            id: 'ID', 
+            parent: 'ParentID', 
+            text: 'Description'        
+        }
+    });
 
     $treeContainer.on('tree-item-selected', function (e, data) {
-
-        vmCreateGrid($("div#treeGrid"), 'Equipment', null, 'EquipmentClassID eq ({0})'.format(data.id));
-
-        $('#removeNode').attr('disabled', false);
-    });
-    
-
-    //hide main treegrid controls and show create node controls
-    $('#createTreeItem').click(function () {
-
-        $('.createNodeControls').show();
-        $('.mainButtonsControls').hide();
-    });
-
-    //creating node
-    $('#accept').click(function () {
-
-        $('.createNodeControls').hide();
-        $('.mainButtonsControls').show();
-
-        var sel = $.jstree.reference('#jstree').get_selected(true)[0] || '#';
-
-        var nodeText = $('#nodeName').val();
-        var parentId = sel ? parseInt(sel.id) : null;
-
-        var node = {
-
-            text: nodeText
-        };
-
-        vmGetMetadata()
-            .done(function (metadata) {
-
-                // find action by name
-                var table = vmGetTables(metadata).filter(function (ind, table) {
-
-                    return table.name == 'EquipmentClass';
-                })
-                .get(0);
-
-                var fields = table.fields.map(function (i, field) {
-
-                    return {
-                        name: field.name,
-                        type: field.type
-                    };
-                }).toArray();
-
-                var data = {};
-
-                for (var i = 0; i < fields.length; i++) {
-                    
-                    if (fields[i].name == 'Description')
-                        data[fields[i].name] = nodeText;
-                    else if (fields[i].name == 'ParentID')
-                        data[fields[i].name] = parentId;
-
-                    else {
-
-                        if (fields[i].type == 'Edm.Int32' || fields[i].type == 'Edm.Single')
-                            data[fields[i].name] = 0;
-                        else if (data[fields[i].name] == 'Edm.String')
-                            data[fields[i].name] = '';
-                    }
-                        
-                };
-                
-                $.ajax({
-                    url: serviceUrl + 'EquipmentClass',
-                    type: "POST",
-                    data: JSON.stringify(data),
-                    contentType: "application/json;odata=verbose"
-                }).success(function () {
-
-                    var new_node = $('#jstree').jstree(true).create_node(sel, node);
-                }).fail(handleError)
-
-            });
         
+        $('div#treeGrid').empty();
+
+        if (data.action != 'delete_node') {
+
+            $('div#treeGrid').jsGrid({
+                height: "500px",
+                width: "1000px",
+
+                sorting: false,
+                paging: true,
+                editing: true,
+                filtering: true,
+                autoload: true,
+                pageLoading: true,
+                inserting: true,
+                pageIndex: 1,
+                pageSize: 10,
+
+            }).jsGrid('initOdata', {
+                serviceUrl: '../../ODataRestierDynamic/api/Dynamic/',
+                table: 'Equipment',
+                defaultFilter: 'EquipmentClassID eq ({0})'.format(data.id)
+            });
+        }
+            
     });
-
-    $('#cancel').click(function () {
-
-        $('.createNodeControls').hide();
-        $('.mainButtonsControls').show();
-    });
-
-    $('#removeNode').click(function () {
-
-        var sel = $.jstree.reference('#jstree').get_selected(true)[0];
-
-        if (sel) {
-
-            if (confirm('Are You sure?')) {
-
-                $.ajax({
-                    type: "DELETE",
-                    url: serviceUrl + 'EquipmentClass' + '(' + sel.id + ')'
-                }).success(function () {
-
-                    ref.delete_node(sel);
-                })
-                .fail(handleError);
-            };
-        };
-    });
-
 
 });
---
+++ ArcelorMittal/Scripts/TreeGrid/treeExt.js
@@ -0,0 +1,232 @@
+﻿(function ($) {
+
+    jQuery.fn.odataTree = function (options) {
+
+        var self = this,
+            serviceUrl = options.serviceUrl,
+            tableName = options.table,
+            keys = options.keys,
+            navigationBar = $('<div />').attr('id', 'navigationBar').appendTo(self),
+            treeRoot = $('<div />').attr('id', 'treeRoot').appendTo(self),
+            mainButtonsRoot = $('<div />').addClass('mainButtonsControls').appendTo(navigationBar),
+            createNodeControlsRoot = $('<div />').addClass('createNodeControls').appendTo(navigationBar).hide(),
+            createBtn = $('<button />').attr('id', 'createTreeItem')
+                                        .addClass('btn')
+                                        .text('Create new node')
+                                        .appendTo(mainButtonsRoot)
+                                        .on('click', vmCreate),
+            renameBtn = $('<button />').attr({
+                                            'id': 'renameNode',
+                                            'disabled': true
+                                        })
+                                        .addClass('btn')
+                                        .text('Rename node')
+                                        .appendTo(mainButtonsRoot)
+                                        .on('click', vmEdit),
+            removeBtn = $('<button />').attr({
+                                            'id': 'removeNode',
+                                            'disabled': true
+                                        })
+                                        .addClass('btn')
+                                        .text('Delete node')
+                                        .appendTo(mainButtonsRoot)
+                                        .on('click', vmRemove),
+            nodeNameInput = $('<input />').attr({
+                                                'id': 'createTreeItem',
+                                                'type': 'text'
+                                            })
+                                            .appendTo(createNodeControlsRoot)
+                                            .on('keyup', vmValidate),
+            acceptBtn = $('<button />').attr({
+                                            'id': 'accept',
+                                            'disabled': true
+                                        })
+                                        .addClass('btn')
+                                        .text('create')
+                                        .appendTo(createNodeControlsRoot)
+                                        .on('click', vmAccept),
+            cancelBtn = $('<button />').attr({ 'id': 'cancel' })
+                                        .addClass('btn')
+                                        .text('cancel')
+                                        .appendTo(createNodeControlsRoot)
+                                        .on('click', vmCancel);
+
+        $.get(serviceUrl + tableName).then(function (data) {
+
+            vmInit(data);
+        });
+
+        function vmInit(data) {
+
+            var treeData = data.value.map(function (item, index, arr) {
+
+                return {
+
+                    'id': item[keys.id],
+                    'parent': item[keys.parent] == null ? '#' : item[keys.parent],
+                    'text': item[keys.text]
+                };
+
+            });
+
+            treeRoot.jstree({
+                'core': {
+
+                    'data': treeData,
+                    'check_callback': true
+                },
+                check_callback: true
+            }).on('changed.jstree', function (e, data) {
+
+                self.trigger('tree-item-selected', { id: data.node.id, action: data.action });
+
+                renameBtn.attr('disabled', false);
+                removeBtn.attr('disabled', false);
+            });
+
+        };
+
+        function vmCreate() {
+
+            mainButtonsRoot.hide();
+            createNodeControlsRoot.show();
+        };
+
+        function vmAccept() {
+
+            mainButtonsRoot.show();
+            createNodeControlsRoot.hide();
+
+            var sel = $.jstree.reference('#treeRoot').get_selected(true)[0] || '#';
+
+            var nodeText = nodeNameInput.val();
+            var parentId = sel ? parseInt(sel.id) : null;
+
+            var node = {
+
+                text: nodeText
+            };
+
+            vmGetMetadata()
+                .done(function (metadata) {
+
+                    var table = vmGetTables(metadata).filter(function (ind, table) {
+
+                        return table.name == tableName;
+                    })
+                    .get(0);
+
+                    var fields = table.fields.toArray();
+
+                    var data = {};
+
+                    for (var i = 0; i < fields.length; i++) {
+
+                        if (fields[i].name == keys.text)
+                            data[fields[i].name] = nodeText;
+                        else if (fields[i].name == keys.parent)
+                            data[fields[i].name] = parentId;
+
+                        else {
+
+                            if (fields[i].type == 'Edm.Int32' || fields[i].type == 'Edm.Single')
+                                data[fields[i].name] = 0;
+                            else if (data[fields[i].name] == 'Edm.String')
+                                data[fields[i].name] = '';
+                        }
+
+                    };
+
+                    $.ajax({
+                        url: serviceUrl + tableName,
+                        type: "POST",
+                        data: JSON.stringify(data),
+                        contentType: "application/json;odata=verbose"
+                    }).success(function (responce) {
+
+                        node.id = responce[keys.id];
+
+                        var ref = treeRoot.jstree(true),
+                                    sel = ref.get_selected();
+                        if (!sel.length) { return false; }
+                        sel = sel[0];
+                        sel = ref.create_node(sel, node);
+
+                        location.reload();
+
+                    }).fail(handleError)
+
+                });
+        };
+
+        function vmEdit() {
+
+            var ref = treeRoot.jstree(true),
+				sel = ref.get_selected();
+
+            if (!sel.length) { return false; }
+            sel = sel[0];
+            ref.edit(sel);
+
+            treeRoot.on('rename_node.jstree', function (e, data) {
+
+                $.get(serviceUrl + tableName + '(' + data.node.id + ')')
+                      .then(function (json) {
+
+                          delete json['@odata.context'];
+                          json[keys.text] = data.text;
+
+                          $.ajax({
+                              url: serviceUrl + tableName + '(' + data.node.id + ')',
+                              type: "PUT",
+                              data: JSON.stringify(json),
+                              contentType: "application/json;odata=verbose"
+
+                          }).fail(handleError);
+                      }).fail(handleError);
+            })
+        };
+
+        function vmRemove() {
+
+            var sel = $.jstree.reference('#treeRoot').get_selected(true)[0];
+
+            if (sel) {
+
+                if (confirm('Are You sure?')) {
+
+                    $.ajax({
+                        type: "DELETE",
+                        url: serviceUrl + tableName + '(' + sel.id + ')'
+                    }).success(function () {
+
+                        var ref = treeRoot.jstree(true),
+                                    sel = ref.get_selected();
+                        if (!sel.length) { return false; }
+                        ref.delete_node(sel);
+
+                    })
+                    .fail(handleError);
+                };
+            };
+        };
+
+        function vmCancel() {
+
+            mainButtonsRoot.show();
+            createNodeControlsRoot.hide();
+        };
+
+        function vmValidate() {
+
+            var val = $(this).val();
+
+            if (val.length > 0)
+                acceptBtn.attr('disabled', false)
+            else
+                acceptBtn.attr('disabled', true)
+        };
+
+    };
+
+})(jQuery);
