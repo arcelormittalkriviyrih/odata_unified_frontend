6acf356d49c333dad45aae5f5dbd499e0b29fb0e
Oleksandr Chekmez <chekmez@nikama.com>
2017-11-03 11:21:25 +0200

    #359 optimize loading of templates list

--- ArcelorMittal/Scripts/App/Market.js
+++ ArcelorMittal/Scripts/App/Market.js
@@ -151,7 +151,7 @@
         $scope.loadingModalData = true;
         $scope.orderCaption = $translate.instant('market.Order.caption.{0}'.format(type));
 
-        var oDataAPI = [indexService.getInfo("Files?$filter=FileType eq 'Excel label' and Status eq '%D0%98%D1%81%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5'")];
+        var oDataAPI = [indexService.getInfo("Files?$select=ID,Name&$filter=FileType eq 'Excel label' and Status eq '%D0%98%D1%81%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5'")];
 
         if (id) {
 
